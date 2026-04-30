078b3e9e6b4d03ff96ce4d5a641abf822851aa94
samurai-labbug <samuelbricenomartinez@gmail.com>
2026-04-30 17:59:02 -0400

    2v
    Se agregro movimiento y ajustar sprite, enemigo

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
 # CONSTANTES
 
-ANCHO, ALTO = 640, 480
+ANCHO, ALTO = 720, 780
 FPS = 60
-TITULO = 'Project Arcade'
+TITULO = 'Project Get that cheese'
 BACK_COLOR = (18, 180, 95)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-PLAYER_IMG = 'src/' 
-ENEMY_IMG = 'src/'
-WIN_IMG = 'src/'
-BG_IMG = 'src/'
-TEXT_FONT = 'src/'
+PLAYER_IMG = 'src/rat_sprite-removebg-preview.png' 
+ENEMY_IMG = 'src/enemyratgame.png'
+WIN_IMG = ''
+BG_IMG = 'src/Woodfloocheeseproye.png'
+TEXT_FONT = 'src\BitcountPropDoubleInk-VariableFont_CRSV,ELSH,ELXP,SZP1,SZP2,XPN1,XPN2,YPN1,YPN2,slnt,wght.ttf'
+CHEESE= 'src/Cheeseratgame-removebg-preview.png'
--- main.py
+++ main.py
@@ -30,22 +30,61 @@ class GameSprite(sprite.Sprite):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
 class Player(GameSprite):
+    def __init__(self, sprite_img, cord_x, cord_y, width, height, speed):
+        super().__init__(sprite_img, cord_x, cord_y, width, height, speed)
+
+        self.original= self.image
+        self.derecha= transform.rotate(self.original,90)
+        self.arriba= transform.rotate(self.original,180)
+        self.izquierda= transform.rotate(self.original,270)
+        
+
     def update(self):
         keys = key.get_pressed()
         if keys[K_d] and self.rect.x <= ANCHO - self.rect.w:
+            self.image= self.derecha
             self.rect.x += self.speed
+            
         elif keys[K_a] and self.rect.x >= 0:
+            self.image= self.izquierda
             self.rect.x -= self.speed
+
+        elif keys[K_s] and self.rect.y < ALTO - self.rect.h:
+            self.image= self.original
+            self.rect.y += self.speed
+
+        elif keys[K_w] and self.rect.y > 0:
+            self.image= self.arriba
+            self.rect.y -= self.speed
+            
+
 
 
 class Enemy(GameSprite):
+    
+
+
     pass
+
+class Items(GameSprite):
+    def __init__(self,sprite_img,cord_x,cord_y, sprite_width , sprite_height , speed, bonus):
+        super().__init__(sprite_img,cord_x,cord_y, sprite_width , sprite_height , speed)
+        self.bonus=bonus
+
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y >= ALTO and self.rect.x>= ANCHO:
+            self.kill()
+
+
 
 
 # OBJETOS
 background = transform.scale(image.load(BG_IMG), (ANCHO, ALTO))
 
-player = Player(PLAYER_IMG, (ANCHO - 80) // 2, ALTO - 70, 80, 60, 5)
+player = Player(PLAYER_IMG, (ANCHO - 80) // 2, ALTO - 70, 70, 90, 5)
+enemy = Enemy(ENEMY_IMG, 344, 300,80,60,5)
+
 # trabajando con grupos:
 
 
@@ -66,7 +105,10 @@ while run:
         screen.blit(background, (0, 0))
         player.reset()
         player.update()
-            
+        enemy.reset()
+        player.update()
+
+
         # CONDICION VICTORIA
         # if victoria:
         #     finish = True
